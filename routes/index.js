f99bbc687bfbc993f236ec74e465328d11583889
ChoiJY <wnsdud0213@ajou.ac.kr>
2018-01-22 18:31:58 +0900

    toady last

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,13 @@
  * @type {*}
  */
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
 var mongoDB = require('../public/javascripts/database');
-var model = require('../public/javascripts/userSchema');
-var uri = 'mongodb://root:0000@ds046267.mlab.com:46267/chatbot';
-
-/**
- * dbconnect
- * @type {*}
- */
-var mongoose = require('mongoose');
 
 // TODO -> multi user에 대한 처리 필요
 var count;
-var tempAry = [, , , ,];
+var tempAry = new Array(4);
 var tempStr;
 var strike;
 var ball;
@@ -31,14 +24,9 @@ const keyboard_numSelectBtn = {
     "buttons": ["쉬움", "어려움", "매우 어려움"]
 };
 
-// TODO
 const keyboard_startBtn = {
     "type": "buttons",
     "buttons": ["숫자 야구 게임", "더치 페이"]
-};
-
-const keyboard_text = {
-    "type": "text"
 };
 
 const message_numHello = {
@@ -51,32 +39,8 @@ const messageBtn_homeLink = {
     "label": "자세한 규칙 확인하기",
     "url": "http://pf.kakao.com/_ZQpzC"
 };
-/* GET home page. */
+/* test 용도 */
 router.get('/', function (req, res, next) {
-    // console.log(mongoDB.getLogs());
-    // res.render('index', {title: 'Express'});
-    // mongoDB.getLogs("12345673").then(function (result) {
-    //     res.send(result);
-    // });
-    checkMyScore("1234567").then(function (result) {
-        res.send(result);
-    });
-    writeMyScore("1234567", "18d");
-    mongoDB.delete("1234567");
-    writeMyScore("1234567", "18d, 계속하기");
-    // console.log(checkMyScore("1234567"));
-    // mongoDB.getLogs("1234567").then(function (results) {
-    //     res.json(checkMyScore("1234567"));
-    // });
-    // mongoDB.save("1234567", "dmddd");
-    // mongoDB.update("1234567", "ddddd");
-
-    // mongoDB.delete("1234567").then(function(res){
-    //     console.log(res)
-    // });
-    // mongoDB.getLogs("1234567").then(function(res){
-    //     console.log(res)
-    // })
 });
 
 // keyboard init
@@ -91,6 +55,7 @@ router.post('/message', function (req, res) {
     var isNumber = false;
 
     // isNumber 체크
+    // TODO 개인화
     tempStr = parseInt(selected.replace(/[^0-9]/g, ''));
 
     if (tempStr >= 0 && tempStr < 100000) {
@@ -100,10 +65,10 @@ router.post('/message', function (req, res) {
         tempAry.sort();
         for (var i = 1; i < tempAry.length; i++) {
             // 같은 숫자 반복
-            if (parseInt(tempAry[i]) == parseInt(tempAry[i - 1])) {
+            if (parseInt(tempAry[i]) === parseInt(tempAry[i - 1])) {
                 break;
             }
-            if (i == tempAry.length - 1) {
+            if (i === tempAry.length - 1) {
                 isNumber = true;
             }
         }
@@ -112,15 +77,14 @@ router.post('/message', function (req, res) {
 
     // 숫자 입력이 아닌 경우
     if (!isNumber) {
-        if (selected == "숫자 야구 게임") {
+        if (selected === "숫자 야구 게임") {
             mongoDB.delete(userKey);
             mongoDB.save(userKey, ["계속하기"]);
             res.json({
                 "message": message_numHello,
                 "keyboard": keyboard_numSelectBtn
             });
-        }
-        else if (selected == "더치 페이") {
+        } else if (selected === "더치 페이") {
             res.json({
                 "message": "아직 완성되지 않은 기능이에요.. TT \n" +
                 "조금만 기다려 주세용(찡긋)\n" +
@@ -128,16 +92,14 @@ router.post('/message', function (req, res) {
                 "keyboard": keyboard_startBtn
             })
             //TODO
-        }
-        else if (selected == "처음으로 돌아가기" || selected == "처음으로") {
+        } else if (selected === "처음으로 돌아가기" || selected === "처음으로") {
             res.json({
                 "message": {
                     "text": "메뉴 선택 화면으로 돌아갑니다"
                 },
                 "keyboard": keyboard_startBtn
             })
-        }
-        else if (selected == "쉬움") {
+        } else if (selected === "쉬움") {
             ranNum = makeRandomNumber(3);
             count = strike = ball = out = 0;
             difficulty = "easy";
@@ -147,8 +109,7 @@ router.post('/message', function (req, res) {
                     "message_button": messageBtn_homeLink
                 }
             })
-        }
-        else if (selected == "어려움") {
+        } else if (selected === "어려움") {
             ranNum = makeRandomNumber(4);
             count = strike = ball = out = 0;
             difficulty = "hard";
@@ -158,8 +119,7 @@ router.post('/message', function (req, res) {
                     "message_button": messageBtn_homeLink
                 }
             })
-        }
-        else if (selected == "매우 어려움") {
+        } else if (selected === "매우 어려움") {
             ranNum = makeRandomNumber(5);
             count = strike = ball = out = 0;
             difficulty = "hell";
@@ -169,7 +129,7 @@ router.post('/message', function (req, res) {
                     "message_button": messageBtn_homeLink
                 }
             })
-        } else if (selected == "기록") {
+        } else if (selected === "기록") {
             checkMyScore(userKey).then(function (results) {
                 // var temp = results.toString().replace(/,/g, '');
                 res.json({
@@ -183,41 +143,14 @@ router.post('/message', function (req, res) {
                     }
                 })
             })
-            // res.json({
-            //     "message": {
-            //         "text": "지금까지의 기록은 아래 버튼을 확인하세요\n" +
-            //         "확인하신 후 계속하기를 눌러주세요"
-            //     },
-            //     "keyboard": {
-            //         "type": "buttons",
-            //         "buttons": checkMyScore(userKey)
-            //     }
-            // })
-        } else if (selected == "계속하기") {
+        } else if (selected === "계속하기") {
             res.json({
                 "message": {
                     "text": "다음 숫자를 입력해주세요!"
                 }
             })
-        }
-
-        // else if (selected == "숫자야구가 뭐죠?") {
-        //     res.json({
-        //         "message": {
-        //             "text": "1. 쉬움은 3자리, 어려움은 4자리, 아주 어려움은 5자리 입니다. \n" +
-        //             "2. 입력하시는 숫자는 0에서 9까지 서로 '다른' 숫자에요.\n" +
-        //             "3. 숫자와 위치가 전부 맞으면 스트라이크!(굿).\n" +
-        //             "4. 숫자는 맞지만 위치가 틀렸을 때는 볼(씨익)",
-        //             "message_button": {
-        //                 "label": "규칙 1도 모를땐 누르세요",
-        //                 "url": "https://namu.wiki/w/%EC%88%AB%EC%9E%90%EC%95%BC%EA%B5%AC"
-        //             }
-        //         },
-        //         "keyboard": keyboard_numSelectBtn
-        //     })
-        // }
-        else {
-            if (selected == "포기") {
+        } else {
+            if (selected === "포기") {
                 res.json({
                     "message": {
                         "text": "처음으로 돌아갑니다"
@@ -234,8 +167,16 @@ router.post('/message', function (req, res) {
     }
     // 숫자 입력
     else {
+        if (!isCorrectNumber(tempStr, difficulty)) {
+            res.json({
+                "message": {
+                    "text": "선택하신 난이도와 자리수가 달라요!\n" +
+                    "알맞게 입력해 주세요"
+                }
+            })
+        }
         if (difficulty == "easy") {
-            var userAry = [, , ,];
+            var userAry = new Array(3);
             var aryLength = userAry.length;
             for (var i = 0; i < aryLength; i++) {
                 userAry[i] = Math.floor(tempStr / Math.pow(10, aryLength - 1 - i));
@@ -243,17 +184,17 @@ router.post('/message', function (req, res) {
             }
         }
         else if (difficulty == "hard") {
-            var userAry = [, , , ,];
+            var userAry = new Array(4);
             var aryLength = userAry.length;
-            for (var i = 0; i < userAry.length; i++) {
+            for (var i = 0; i < aryLength; i++) {
                 userAry[i] = Math.floor(tempStr / Math.pow(10, aryLength - 1 - i));
                 tempStr = (tempStr % Math.pow(10, aryLength - 1 - i));
             }
         }
         else if (difficulty == "hell") {
-            var userAry = [, , , , ,];
+            var userAry = new Array(5);
             var aryLength = userAry.length;
-            for (var i = 0; i < userAry.length; i++) {
+            for (var i = 0; i < aryLength; i++) {
                 userAry[i] = Math.floor(tempStr / Math.pow(10, aryLength - 1 - i));
                 tempStr = (tempStr % Math.pow(10, aryLength - 1 - i));
             }
@@ -269,15 +210,15 @@ router.post('/message', function (req, res) {
         }
         // strike / ball / out
         for (var i = 0; i < aryLength; i++) {
-            if (ranNum.indexOf(userAry[i]) != -1) {
-                if (ranNum[i] == userAry[i]) strike += 1;
+            if (ranNum.indexOf(userAry[i]) !== -1) {
+                if (ranNum[i] === userAry[i]) strike += 1;
                 else ball += 1;
             }
             else out += 1;
         }
         count += 1;
         // 정답
-        if (strike == aryLength) {
+        if (strike === aryLength) {
             mongoDB.delete(userKey).then(function (results) {
             });
             res.json({
@@ -294,15 +235,10 @@ router.post('/message', function (req, res) {
         else {
             var temp = userAry.toString().replace(/,/g, '');
             writeMyScore(userKey, temp + " " + "" + strike + "S " + ball + "B").then(function (results) {
-                });
+            });
             res.json({
                 "message": {
-                    "text": // ranNum + " | " + userAry \n+
-                    // "현재 " + count + "번째 도전!\n"
-                    // + Strike + " :Strike\n"
-                    // + Ball + " :Ball\n"
-                    "" + strike + "S " + ball + "B"
-                    // + out + "Out 입니다."
+                    "text": strike + " S " + ball + " B"
                 }
             })
         }
@@ -318,24 +254,49 @@ router.post('/message', function (req, res) {
  *
  * Des
  * 배열 내에 중복된 수가 있는지에 대한 검사
- * @param inputArray
- * @returns 중복 시 true,
- *          아닐 시 false
+ * @param inputArray (Array)
+ * @returns boolean
  */
 function isDuplicate(inputArray) {
     var tempAry;
     tempAry = inputArray.slice(0, inputArray.length - 1);
     tempAry.sort();
     for (var i = 1; i < tempAry.length; i++) {
-        if (parseInt(tempAry[i]) == parseInt(tempAry[i - 1])) {
+        if (parseInt(tempAry[i]) === parseInt(tempAry[i - 1])) {
             return true;
         }
-        if (i == (tempAry.length - 1)) {
+        if (i === (tempAry.length - 1)) {
             return false;
         }
     }
 }
-
+/**
+ * isCorreceNumber
+ *
+ * Des
+ * 난이도에 따른 입력 숫자 자리가 맞지 않는지 확인
+ * @params inputString (String)
+ *         difficulty (String)
+ * @returns boolean
+ */
+function isCorrectNumber(inputString, difficulty) {
+    if (difficulty === "easy") {
+        if (inputString.length !== 3) {
+            return false;
+        }
+        else return true;
+    } else if (difficulty === "hard") {
+        if (inputString.length !== 4) {
+            return false;
+        }
+        else return true;
+    } else {
+        if (inputString.length !== 5) {
+            return false;
+        }
+        else return true;
+    }
+}
 /**
  * makeRandomNumber
  *
@@ -349,9 +310,9 @@ function isDuplicate(inputArray) {
  */
 function makeRandomNumber(type) {
     var temp;
-    if (type == 4) var output = [, , , ,];
-    else if (type == 3) var output = [, , ,];
-    else var output = [, , , , ,];
+    if (type === 4) var output = new Array(4);
+    else if (type === 3) var output = new Array(3);
+    else var output = new Array(5);
     for (var i = 0; i < output.length; i++) {
         output[i] = getRandomIntInclusive(0, 9);
     }
@@ -361,11 +322,11 @@ function makeRandomNumber(type) {
     console.log("why sorted : " + output);
     for (var i = 1; i < temp.length; i++) {
         // 같은 숫자 반복
-        if (temp[i] == temp[i - 1]) {
+        if (temp[i] === temp[i - 1]) {
             console.log("temp  " + i + "와 i-1 중복 발생: " + temp);
             return makeRandomNumber(temp.length);
         }
-        if (i == (temp.length - 1)) return output;
+        if (i === (temp.length - 1)) return output;
     }
 }
 
@@ -382,27 +343,6 @@ function checkMyScore(user_key) {
             res(results);
         })
     });
-    // var userKey = user_key;
-    // var tempAry = [];
-    // var test = 1;
-    // var test2 = mongoDB.getLogs(userKey).then(function (results) {
-    //     tempAry = results;
-    //     console.log("1:" + test);
-    //     test = 2;
-    //     console.log("2:"+test);
-    //     return test;
-    // });
-    // console.log("3 :"+test);
-    // console.log("test2 : "+test2)
-    // // if (mongoDB.getLogs(userKey) != "error") {
-    // //     tempAry = tempAry.concat(mongoDB.getLogs(userKey));
-    // //     return tempAry;
-    // // }
-    // // else {
-    // //     tempAry = tempAry.concat("현재 기록이 없으시네요(멘붕)");
-    // //     return tempAry;
-    // // }
-    // return tempAry;
 }
 
 /**
@@ -412,7 +352,7 @@ function checkMyScore(user_key) {
 function writeMyScore(userKey, data) {
     return new Promise(function (res) {
         mongoDB.getLogs(userKey).then(function (results) {
-            if (results == "not exist") {
+            if (results === "not exist") {
                 mongoDB.save(userKey, data);
             }
             else {
@@ -422,26 +362,8 @@ function writeMyScore(userKey, data) {
             }
         })
     })
-    // mongoDB.getLogs(userKey).then(function (results) {
-    //     if (results == "not exist") {
-    //         mongoDB.save(userKey, data);
-    //     }
-    //     else {
-    //         mongoDB.update(userKey, data).then(function (results) {
-    //             console.log("writeMyScore's results " + results);
-    //         })
-    //     }
-    // });
-    // if (mongoDB.getLogs(userKey) == "not exist") {
-    //     mongoDB.save(userKey, data);
-    // }
-    // else mongoDB.update(userKey, data);
-}
+};
 
-/**
- *
- * @type {string}
- */
 const message_gameRule = "\n[게임설명]\n" +
     "- 숫자야구는 정한 난이도에 맞는 숫자조합을 맞추는 게임입니다.\n" +
     "- 숫자와 자리가 일치 시에는 1Strike 증가, 숫자만 일치하고 자리는 불일치시 1Ball증가합니다.\n\n" +
